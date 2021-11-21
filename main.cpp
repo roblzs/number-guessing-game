2e0f3c90026a5cf77c5d24846a8f8c7f211ecc4d
Rob <robzlegz@gmail.com>
2021-11-21 12:45:11 +0200

    ready game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 int generate_random_number(){
@@ -14,10 +15,47 @@ int generate_random_number(){
     return random_number;
 }
 
+bool check_if_equal(int& correct_number, int& guess){
+    bool correct = false;
+
+    if(guess == correct_number){
+        correct = true;
+    }else if(guess > correct_number){
+        cout << "Correct number is smaller than " << guess << endl;
+    }else if(guess < correct_number){
+        cout << "Correct number is bigger than " << guess << endl;
+    }
+
+    return correct;
+}
+
+int guess_the_number(int& correct_number){
+    int guess = 0;
+
+    cout << endl << "Enter Your guess: ";
+    cin >> guess;
+    
+    return guess;
+}
+
 int main(){
     int random_number = generate_random_number();
 
+    cout << "You have 10 attempts to guess the number" << endl;
 
+    int attempts = 10;
+    bool correct = false;
 
-    
+    while(attempts > 0 && !correct){
+        int guess = guess_the_number(random_number);
+        correct = check_if_equal(random_number, guess);
+
+        attempts --;
+    }
+
+    if(!correct){
+        cout << endl << "You lose!";
+    }else{
+        cout << endl << "You win!";
+    }
 }
