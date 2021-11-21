9d3429a6d2b38feb82c927283f87792b7ff09e98
Rob <robzlegz@gmail.com>
2021-11-21 12:06:06 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int generate_random_number(){
+    int MIN_INT, MAX_INT;
+
+    MIN_INT = 0;
+    MAX_INT = 100;
+
+    srand(time(0));
+
+    
+}
+
+int main(){
+   
+
+
+}
